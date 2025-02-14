7d5466d211ee0587b3e36a3b86a86e71386e856c
omkar1010 <102678603+omkar1010@users.noreply.github.com>
2025-02-14 16:52:12 +0530

    2d arr

---
+++ 2darr.js
@@ -0,0 +1,14 @@
+arr =[[1,2],[3,4],[5,6]]
+
+let row  = 2;
+let column = 3
+
+for(let i=0;i<column;i++){
+    let string  = ""
+    for(let j=0; j<row; j++){
+        string += arr[i][j] + " "
+
+    }
+            console.log(string)
+    
+}
