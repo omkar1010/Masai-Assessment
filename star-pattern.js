df54c2735f5781869ee76f33e5c9be609346c072
omkar1010 <102678603+omkar1010@users.noreply.github.com>
2025-02-14 18:58:51 +0530

    star pattern code

---
+++ star-pattern.js
@@ -0,0 +1,13 @@
+
+let  n=7;
+
+let mid = n - 2;
+let string = ""
+for(let i=0;i < n; i++){
+  string += "* "
+}
+console.log(string)
+for(let j=0; j < mid; j++){
+  console.log("*")
+}
+console.log(string)
